4b46a61ba3c52c896c76caab1825e2c3f9d394fd
Jonathan LeBlanc <joncleblanc@yahoo.com>
2010-10-19 14:36:48 -0700

    added readme + changed $_POST / $_GET array merge to $_REQUEST

--- opensocial-gadgets/validating_signed_requests_hmac/signed_requests_validate.php
+++ opensocial-gadgets/validating_signed_requests_hmac/signed_requests_validate.php
@@ -5,7 +5,7 @@ $key = 'KEY HERE';
 $secret = 'KEY HERE';
 
 //Build a request object from the current request
-$request = OAuthRequest::from_request(null, null, array_merge($_GET, $_POST));
+$request = OAuthRequest::from_request(null, null, $_REQUEST);
 $consumer = new OAuthConsumer($key, $secret, NULL);
 
 //Initialize signature method
