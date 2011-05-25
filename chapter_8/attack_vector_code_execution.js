88eb50fe4ba9a1b22f5d876e58819bcc5c85fa37
Jonathan LeBlanc <joncleblanc@yahoo.com>
2011-05-25 16:42:13 -0700

    chapter 8 examples

---
+++ chapter_8/attack_vector_code_execution.js
@@ -0,0 +1,8 @@
+var script = document.createElement("script");
+script.appendChild(
+   document.createTextNode(
+      var userCookie = document.cookie;
+      //use user cookies
+   )
+);
+document.body.appendChild(script);
