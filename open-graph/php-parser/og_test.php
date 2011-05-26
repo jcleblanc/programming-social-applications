caf7cccdcbd30dba2e0ca5d604c659772911ee77
Jonathan LeBlanc <joncleblanc@yahoo.com>
2011-05-25 22:25:55 -0700

    moved open graph parser

--- open-graph/php-parser/og_test.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once('OpenGraph.php');
-
-$url = 'http://www.yelp.com/biz/the-restaurant-at-wente-vineyards-livermore-2';
-$graph = new OpenGraph($url);
-print_r($graph->get_one('title'));
-print_r($graph->get_all());
-
-?>
--- open-graph/python-parser/index.py
+++
@@ -1,7 +0,0 @@
-from OpenGraph import OpenGraphParser
-
-url = 'http://www.nhl.com/ice/player.htm?id=8468482';
-og_instance = OpenGraphParser(url)
-
-print og_instance.get_one('description')
-print og_instance.get_all()
